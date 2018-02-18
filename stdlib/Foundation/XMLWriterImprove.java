ddc7f963375869fdacf37e858946682047a46ee2
mistermboy <pabloyo97@hotmail.com>
2018-02-18 16:45:54 +0100

    started improvement

--- stdlib/Foundation/XMLWriterImprove.java
+++ stdlib/Foundation/XMLWriterImprove.java
@@ -2,7 +2,6 @@ package Foundation;
 
 import java.io.FileWriter;
 import java.io.PrintWriter;
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -10,14 +9,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Map;
 
-import javax.lang.model.type.TypeVariable;
-
-import org.jdom2.Document;
-import org.jdom2.Element;
-import org.jdom2.output.Format;
-import org.jdom2.output.XMLOutputter;
-
-public class XMLWriter {
+public class XMLWriterImprove {
 
 	public static <T> void _write(ArrayList<T> objects, String root, String documentName) {
 		FileWriter fichero = null;
@@ -136,55 +128,6 @@ public class XMLWriter {
 
 	private static String tab() {
 		return "\t";
-	}
-
-	@SuppressWarnings("unchecked")
-	public static <T> void test(ArrayList<T> objects, String root, String documentName) {
-		try {
-
-			Element top = new Element(root);
-			Document doc = new Document(top);
-
-			for (Object o : objects) {
-				Class<?> clazz = o.getClass();
-
-				Element obj = new Element(o.getClass().getSimpleName());
-
-				for (Field field : clazz.getDeclaredFields()) {
-					String name = field.getName();
-					String value = "";
-					Class<?>[] colection = null;
-					for (Method method : clazz.getMethods()) {
-						if (method.getName().toLowerCase().endsWith("get" + name.toLowerCase())) {
-							try {
-								if (isClassCollection(field.getType())) {
-
-									Object[] a = (Object[]) method.invoke(o);
-									for (Object ob : a) {
-										obj.addContent(new Element(name).setText(ob.toString()));
-									}
-								} else {
-									value = String.valueOf(method.invoke(o));
-								}
-							} catch (IllegalAccessException e) {
-								System.out.println("Could not determine method: " + method.getName());
-							} catch (InvocationTargetException e) {
-								System.out.println("Could not determine method: " + method.getName());
-							}
-						}
-					}
-					if (!isClassCollection(field.getType())) {
-						obj.addContent(new Element(name).setText(value));
-					}
-				}
-				doc.getRootElement().addContent(obj);
-			}
-			XMLOutputter output = new XMLOutputter();
-			output.setFormat(Format.getPrettyFormat());
-			output.output(doc, new FileWriter(documentName + ".xml"));
-		} catch (Exception e) {
-			System.out.println("Something happens");
-		}
 	}
 
 }
